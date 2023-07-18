8cb9e1f30917cbfe0b0521d84aa4568202daade1
Mehmet Eyüpoğlu <59320966+mehmeteyupoglu@users.noreply.github.com>
2023-07-18 16:49:07 +0300

    feat: test post and get requests

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mysql from "mysql";
 
 const app = express();
+app.use(express.json());
 
 const db = mysql.createConnection({
   host: "localhost",
@@ -16,11 +17,22 @@ app.get("/", (req, res) => {
 
 app.get("/books", (req, res) => {
   const q = "SELECT * FROM books";
+
   db.query(q, (err, data) => {
     if (err) return res.json("Error fetching data");
     return res.json(data);
   });
 });
+
+app.post("/books", (req, res) => {
+  const q = "INSERT INTO books (`title`, `desc`, `cover`) VALUES (?)";
+  const values = [req.body.title, req.body.desc, req.body.cover];
+  db.query(q, [values], (err, data) => {
+    if (err) return res.json(err);
+    return res.json(data);
+  });
+});
+
 app.listen(8800, () => {
   console.log("connected to backend");
 });
